cd33946b949e17723d3f085070563aecf41aa0a5
pdi20161 <pdi2016.1@gmail.com>
2016-03-12 13:51:53 -0300

    trocaregioes

--- codigos/trocaregioes.cpp
+++ codigos/trocaregioes.cpp
@@ -18,28 +18,31 @@ int main(int argc, char** argv){
   width=image.size().width;
   height=image.size().height;
 
-  Mat imgRect1(image, Rect(0,0,width/2-1,height/2-1));
-  Mat imgRect2(image, Rect(width/2,0,width-1,height/2-1));
-  //Mat imgRect3(image, Rect(height/2,0,height-1,width/2-1));
-  //Mat imgRect4(image, Rect(height/2,width/2,height-1,width-1));
-  // for(int i=0; i<height; i++){
-  //   for(int j=0; j<width; j++){
-  //     if(i <=height/2-1 || j<=width/2-1 ){
-	//        image.at<uchar>(i,j) = imgRect4.at<uchar>(i,j);
-  //     }
-  //   }
-  // }
+  Mat imgRect1(image, Rect(0,0,width/2,height/2));
+  Mat imgRect2(image, Rect(width/2-1,0,width/2,height/2));
+  Mat imgRect3(image, Rect(0,height/2-1,width/2,height/2));
+  Mat imgRect4(image, Rect(width/2-1,height/2-1,height/2,width/2));
   newimage = image.clone();
-  imgRect1.copyTo(newimage(Rect(width/2+1,0,imgRect1.size().height,imgRect1.size().width)));
+
+  
+
+  imgRect1.copyTo(newimage(Rect(width/2,0,imgRect1.size().height,imgRect1.size().width)));
+  imgRect2.copyTo(newimage(Rect(width/2,height/2,imgRect2.size().height,imgRect2.size().width)));
+  imgRect3.copyTo(newimage(Rect(0,height/2,imgRect3.size().height,imgRect3.size().width)));
+  imgRect4.copyTo(newimage(Rect(0,0,imgRect4.size().height,imgRect4.size().width)));
   // imgRect2.copyTo(image);
   namedWindow("Original",WINDOW_AUTOSIZE);
-  imshow("Original", imgRect2);
-  waitKey();
-  namedWindow("Original",WINDOW_AUTOSIZE);
-  imshow("Original", imgRect1);
-  waitKey();
-  namedWindow("Original",WINDOW_AUTOSIZE);
-  imshow("Original", newimage);
+  imshow("Original", image);
+  namedWindow("rect1",WINDOW_AUTOSIZE);
+  imshow("rect1", imgRect1);
+  namedWindow("rect2",WINDOW_AUTOSIZE);
+  imshow("rect2", imgRect2);
+  namedWindow("rect3",WINDOW_AUTOSIZE);
+  imshow("rect3", imgRect3);
+  namedWindow("rect4",WINDOW_AUTOSIZE);
+  imshow("rect4", imgRect4);
+  namedWindow("Saida",WINDOW_AUTOSIZE);
+  imshow("Saida", newimage);
   waitKey();
   return 0;
 }
